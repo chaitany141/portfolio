3799720d5e81492e753e36328500a04c8e80431f
chaitany141 <chaitanyapawar54321@gmail.com>
2026-02-28 16:33:16 +0530

    Initial commit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,15 +8,16 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            Hi everyone! I’m <span className="purple">Chaitanya Pawar</span>{" "}
+            from <span className="purple">Pune, India</span>.
             <br />
-            I’m currently working as a{" "}
-            <span className="purple">Software Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
-            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-            <span className="purple">Mathematics and Computing</span> from{" "}
-            <span className="purple">BIT Mesra</span>.
+            I’m currently pursuing a Bachelor’s degree in Computer Engineering at 
+            <span className="purple"> Pune Institute Of Computer Technology</span> {" "}
+            and I'm passionate about 
+                        <span className="purple"> full-stack development </span>
+ and  <span className="purple">problem solving</span>.
+
+      
             <br />
             <br />
             Outside of coding, I love engaging in activities that keep me
@@ -38,7 +39,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Chaitanya</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import macOs from "../../Assets/TechIcons/Apple MacOSX.svg";
+// import macOs from "../../Assets/TechIcons/Apple MacOSX.svg";
 import chrome from "../../Assets/TechIcons/Google Chrome.svg";
 import vsCode from "../../Assets/TechIcons/vscode.svg";
 import intelliJ from "../../Assets/TechIcons/intellij-idea.svg";
@@ -8,10 +8,10 @@ import intelliJ from "../../Assets/TechIcons/intellij-idea.svg";
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      {/* <Col xs={4} md={2} className="tech-icons">
         <img src={macOs} alt="macOs" className="tech-icon-images" />
         <div className="tech-icons-text">Mac Os</div>
-      </Col>
+      </Col> */}
       <Col xs={4} md={2} className="tech-icons ">
         <img src={chrome} alt="Chrome" className="tech-icon-images" />
         <div className="tech-icons-text">Google Chrome</div>
